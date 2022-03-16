d1c95ac19dcb67972f0c48bfe0599046b62cb6db
Oman Ramalho <omanapple42@hotmail.com>
2022-03-15 22:57:47 -0300

    finished upload files

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -3,6 +3,12 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import product from './product'
+import user from './user'
+import order from './order'
+import orderItem from  './orderItem'
+import paymentResult from './paymentResult'
+import shippingAddress from './shippingAddress'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,5 +18,11 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    product,
+    user,
+    order,
+    orderItem,
+    paymentResult,
+    shippingAddress,
   ]),
 })
